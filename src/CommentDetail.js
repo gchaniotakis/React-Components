a1d3cfaaec5d0976fa07835040669f8cfd622324
George Chaniotakis <haniotinho@gmail.com>
2019-06-22 01:30:58 +0300

    nesting components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import faker from 'faker';
 
 const CommentDetail = () => {
     return (
@@ -19,3 +20,5 @@ const CommentDetail = () => {
 
     );
 };
+
+export default CommentDetail;
