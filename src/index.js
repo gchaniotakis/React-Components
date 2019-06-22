84d4fc26a101a5499ca41e48695981f10779cd94
George Chaniotakis <haniotinho@gmail.com>
2019-06-23 01:49:07 +0300

    creating approval card

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
 const App = () =>{
     return (
        <div className = "ui container comments">
+           <br></br>
+           <ApprovalCard/>
            <CommentDetail  
                 author = "Sam" 
                 timestamp = "Today at 17:00" 
@@ -17,13 +19,13 @@ const App = () =>{
                 timestamp = "Today at 09:00" 
                 content = "Hell yeah!"
                 avatar = {faker.image.avatar()}
-                />             
+            />             
            <CommentDetail  
                 author = "Anne" 
                 timestamp = "Yesterdat at 19:00" 
                 content = "Well done!"
                 avatar = {faker.image.avatar()}
-                />   
+            />   
        </div> 
     );
 };
