6c41ad979f9327e04e6a30214c6439fe32a70d30
George Chaniotakis <haniotinho@gmail.com>
2019-06-23 00:25:39 +0300

    multiple props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -6,16 +6,16 @@ const CommentDetail = (props) => {
     return (
         <div className = "comment">
                <a href = "/" className = "avatar">
-                   <img alt="avatar" src = {faker.image.avatar()}/>
+                   <img alt="avatar" src = {props.avatar}/>
                </a>
                <div className = "content">
                    <a href = "/" className = "author">
                        {props.author}
                    </a>
                    <div className = "metadata">
-                       <span className = "date">Today at 18:00</span>
+                       <span className = "date">{props.timestamp}</span>
                    </div>
-                   <div className = "text">Nice one!</div>
+                   <div className = "text">{props.content}</div>
                </div>
            </div>
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () =>{
     return (
        <div className = "ui container comments">
-           <CommentDetail  author = "Sam"/>           
+           <CommentDetail  
+                author = "Sam" 
+                timestamp = "Today at 17:00" 
+                content = "Nice one!"
+                avatar = {faker.image.avatar()}
+            /> 
+           <CommentDetail  
+                author = "Lee" 
+                timestamp = "Today at 09:00" 
+                content = "Hell yeah!"
+                avatar = {faker.image.avatar()}
+                />             
+           <CommentDetail  
+                author = "Anne" 
+                timestamp = "Yesterdat at 19:00" 
+                content = "Well done!"
+                avatar = {faker.image.avatar()}
+                />   
        </div> 
     );
 };
